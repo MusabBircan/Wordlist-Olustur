0a0951336e3ded737c7048c5e3cb3610e8f180a4
Your Name <you@example.com>
2020-09-17 18:49:26 +0300

    project pertama

---
+++ Lib/kayit.php
@@ -0,0 +1,26 @@
+
+	<?php
+	$ad = '';
+	$soyadad = '';
+	$lakap = '';
+	$anne = '';
+	$baba = '';
+	$kardes = '';
+	$sevgili = '';
+	$sevgilisoyad = '';
+	$dogumtarihi = '';
+	$dogumyili = '';
+	$cikmayili = '';
+	$cikmatarihi = '';
+	$sehir = '';
+	$takim = '';
+	$takimtarihi = '';
+	$takimkisa = '';
+	$plaka = '';
+	$eskisifre = '';
+	$tel = '';
+	$annetel = '';
+	$babatel = '';
+	$kardestel = '';
+	$sevgilitel = '';
+	$tckimlikno = '';?>
